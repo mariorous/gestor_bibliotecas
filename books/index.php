fa7e1826c721af7d1e472664b47002f7de6db21d
〽️ario <emariors@gmail.com>
2024-10-23 17:14:45 +0200

    build: :construction_worker: Init vistas

---
+++ books/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Libros</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <h1>Listado de Libros</h1>
+    <a href="createBook.php">Añadir Nuevo Libro</a>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,34 @@
+<?php
 
+require_once 'db/db.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Gestión de Bibliotecas</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <h1>Gestión Centralizada de Bibliotecas</h1>
+
+    <!-- Buscador de libros -->
+    <form method="" action="">
+        <label for="search">Buscar libros por título, autor o ISBN:</label>
+        <input type="text" id="search" name="search" value="">
+        <button type="submit">Buscar</button>
+    </form>
+
+
+    <hr>
+
+    <!-- Enlaces a la gestión de bibliotecas y libros -->
+    <h2>Gestión</h2>
+    <ul>
+        <li><a href="libraries/index.php">Gestionar Bibliotecas</a></li>
+        <li><a href="books/index.php">Gestionar Libros</a></li>
+    </ul>
+</body>
+</html>
