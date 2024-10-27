a37f9244d7a43e5346f1c442096f5baadf7855e9
Sandra Borges <106088439+sborgesm@users.noreply.github.com>
2024-10-27 16:23:46 +0100

    Actualización Index Librerias

--- libraries/index.php
+++ libraries/index.php
@@ -15,28 +15,52 @@ $resultado = mysqli_query($conn, $queryMostrar);
 <head>
     <meta charset="UTF-8">
     <title>Bibliotecas</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/libraries.css">
 </head>
 <body>
     <h1>Listado de Bibliotecas</h1>
-    <a class="createButton" href="createLibrary.php">Añadir Nueva Biblioteca</a>
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>NAME</th>
-            <th>ADDRESS</th>
-            <th>PHONE</th>
-            <th>ACCIONES</th>
-        </tr>
-        <?php while ($row = mysqli_fetch_assoc($resultado)) : ?>
-            <tr>
-                <td><?php echo $row['id']; ?></td>
-                <td class="textLeft"><?php echo $row['name']; ?></td>
-                <td class="textLeft"><?php echo $row['address']; ?></td>
-                <td><?php echo $row['phone']; ?></td>
-                <td></td>
-            </tr>
-        <?php endwhile; ?>
-    </table>
+    <div class="container">
+        <div class="divTable">
+            <table>
+                <tr>
+                    <th class="tableHeaderLeft">ID</th>
+                    <th>NOMBRE</th>
+                    <th>DIRECCIÓN</th>
+                    <th>TELÉFONO</th>
+                    <th class="tableHeaderRight">ACCIONES</th>
+                </tr>
+                <?php while ($row = mysqli_fetch_assoc($resultado)) : ?>
+                    <tr>
+                        <td><?php echo $row['id']; ?></td>
+                        <td class="textLeft"><?php echo $row['name']; ?></td>
+                        <td class="textLeft"><?php echo $row['address']; ?></td>
+                        <td><?php echo $row['phone']; ?></td>
+                        <td class="rowButtons">
+                            <form action="editLibrary.php" method="POST">
+                                <input type="hidden" name="id" value="<?php echo $row['id'] ?>">
+                                <button type="submit" class="editLibrary">EDIT</button>
+                            </form>
+                            <form action="deleteLibrary.php" method="POST">
+                                <input type="hidden" name="id" value="<?php echo $row['id'] ?>">
+                                <button type="submit" class="deleteLibrary">DELETE</button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endwhile; ?>
+            </table>
+        </div>
+        <div class="divForm">
+            <h2>Añadir Bibliotecas</h2>
+            <form action="createLibrary.php" method="POST">
+                <label for="name">Nombre:</label>
+                <input type="text" id="name" name="name" required>
+                <label for="address">Dirección:</label>
+                <input type="text" id="address" name="address" required>
+                <label for="phone">Teléfono:</label>
+                <input type="text" id="phone" name="phone" required>
+                <input type="submit" value="Crear" name="addLibrary">
+            </form>
+        </div>  
+    </div>
 </body>
 </html>
